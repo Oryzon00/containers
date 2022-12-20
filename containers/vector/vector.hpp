ea3f9b583ff066694471dbd72c154bfb5f3da2ea
Adrian Jung <adrian.mael.jung@gmail.com>
2022-12-20 20:24:53 +0100

    work

--- containers/vector/vector.hpp
+++ containers/vector/vector.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
 #include <memory>
-
-template < class T > class vector_it;
+#include <exception>
 
 namespace ft
 {
@@ -13,18 +12,27 @@ namespace ft
 
 /*------------------------------------------------------------------------------------------------*/
 			/* TYPEDEF */
-			typedef	T								value_type;
-			typedef	Alloc							allocator_type;
-			typedef	allocator_type::reference		reference;
-			typedef	allocator_type::const_reference	const_reference;
-			typedef	allocator_type::pointer			pointer;
-			typedef	allocator_type::const_pointer	const_pointer;
-			typedef	
+			typedef	T											value_type;
+			typedef	Alloc										allocator_type;
+			typedef	allocator_type::reference					reference;
+			typedef	allocator_type::const_reference				const_reference;
+			typedef	allocator_type::pointer						pointer;
+			typedef	allocator_type::const_pointer				const_pointer;
+			typedef	random_access_iterator<value_type>			iterator;
+			typedef	random_access_iterator<const value_type>	const_iterator;
+			typedef	random_access_iterator...					reverse_iterator;
+			typedef	random_access_iterator....					const_reverse_iterator;
 
 /*------------------------------------------------------------------------------------------------*/			
 		private:
+			/* ATTRIBUT MEMBRE */
+			T*				_array;
+			size_t			_currentsize;
+			size_t			_maxSize;
+			allocator_type	_allocator
 
-		protected:
+/*------------------------------------------------------------------------------------------------*/
+		public:
 
 	};
 };
