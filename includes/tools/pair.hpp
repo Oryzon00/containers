d8766034110c2f040da5ba4848af2b768ee2800e
Adrian Jung <adrian.mael.jung@gmail.com>
2023-02-02 18:01:32 +0100

    pair // make pair

--- includes/tools/pair.hpp
+++ includes/tools/pair.hpp
@@ -16,8 +16,11 @@ struct pair
 {
 	typedef	T1	first_type;
 	typedef	T2	second_type;
+
 	T1	first;
 	T2	second;
+
+	/* ---------- CONSTRUCTORS ----------- */
 
 	/* Default constructor */
 	pair() : first(), second ()		{};
@@ -27,27 +30,79 @@ struct pair
 
 
 	/* Copy constructor */
-	// template dans template ? mmhhh.....
 	template <class U1, class U2>
-	pair(const pair <U1, U2> & p) : first(p.U1), second(p.U2)	{};
+	pair(const pair <U1, U2> & p) : first(p.first), second(p.second)	{};
 
+	/* ---------- OPERATOR OVERLOAD ---------- */
 
-
-
+	/* Operator = */
+	pair &	operator=(const pair & p)
+	{
+		first = p.first;
+		second = p.second;
+		return (*this);
+	}
 };
 
+/* ---------- NON MEMBER FUNCTIONS ---------- */
 
+/* Operator == */
+template <class T1, class T2>
+  bool operator== (const pair<T1,T2> & lhs, const pair<T1,T2> & rhs)
+{
+	return lhs.first==rhs.first && lhs.second==rhs.second;
+}
 
+/* Operator != */
+template <class T1, class T2>
+  bool operator!= (const pair<T1,T2> & lhs, const pair<T1,T2> & rhs)
+{
+	return !(lhs==rhs);
+}
+
+/* Operator < */
+template <class T1, class T2>
+  bool operator<  (const pair<T1,T2> & lhs, const pair<T1,T2> & rhs)
+{
+	return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second);
+}
+
+/* Operator <= */
+template <class T1, class T2>
+  bool operator<= (const pair<T1,T2> & lhs, const pair<T1,T2> & rhs)
+{
+	return !(rhs<lhs);
+}
+
+/* Operator > */
+template <class T1, class T2>
+  bool operator>  (const pair<T1,T2> & lhs, const pair<T1,T2> & rhs)
+{
+	return rhs<lhs;
+}
+
+/* Operator >= */
+template <class T1, class T2>
+  bool operator>= (const pair<T1,T2> & lhs, const pair<T1,T2> & rhs)
+{
+	return !(lhs<rhs);
+}
 
 
 /*------------------------------------------------------------------------------------------------*/
 
 /*
 ----- MAKE_PAIR -----
-
+Construct pair object
+Constructs a pair object with its first element set to x and its second element set to y.
 */
 
+template <class T1,class T2>
+pair<T1,T2>	make_pair (T1 x, T2 y)
+{
+	return ( pair<T1,T2> (x, y) );
+}
 
-
+/*------------------------------------------------------------------------------------------------*/
 
 }
--- includes/tools/test.cpp
+++ includes/tools/test.cpp
@@ -1,21 +1,27 @@
 #include "type_traits.hpp"
+#include "pair.hpp"
 #include <iostream>
+
+#include <utility>
 
 int main(void)
 {
 	std::cout << "NO REMOVE CV" << std::endl;
+
 	std::cout << "cons_bool: " << (ft::is_integral_base<const bool>::value)	<< std::endl;
 	std::cout << "bool: " << (ft::is_integral_base<bool>::value)			<< std::endl;
 
 	std::cout << std::endl << "-----------------" << std::endl << std::endl;
 
 	std::cout << "YES REMOVE CV" << std::endl;
+
 	std::cout << "cons_bool: " << (ft::is_integral<const bool>::value)	<< std::endl;
 	std::cout << "bool: " << (ft::is_integral<bool>::value)			<< std::endl;
 
 	std::cout << std::endl << "-----------------" << std::endl << std::endl;
 
 	std::cout << "SWAP" << std::endl;
+
 	int a = 1;
 	int b = 2;
 	std::cout << "a = " << a << " /// b = " << b << std::endl;
@@ -24,4 +30,25 @@ int main(void)
 
 	std::cout << std::endl << "-----------------" << std::endl << std::endl;
 
+
+	std::cout << "PAIR" << std::endl;
+
+	ft::pair<std::string, int>	p1("hola", -1);
+	ft::pair<std::string, unsigned int> p2(p1);
+	std::cout << "p1.second : " << p1.second << std::endl;
+	std::cout << "p2.second : " << p2.second << std::endl;
+	std::cout << std::endl;
+
+	ft::pair <int, int> foo;
+	foo = ft::make_pair(1, 42);
+	std::cout << "make_pair : ";
+	std:: cout << "first : " << foo.first <<  " //// second : " << foo.second << std::endl;
+	std::cout << std::endl;
+
+	ft::pair<int, int> bar = ft::make_pair(2, 42);
+	bool output = foo < bar;
+	std::cout << "comparaison: foo < bar = " << output << std::endl;
+	std::cout << std::endl << "-----------------" << std::endl << std::endl;
+	
+	
 }
