2f25f7d00defebb7bc256dd3aa761825b72a0f7e
Adrian Jung <adrian.mael.jung@gmail.com>
2023-02-10 18:35:45 +0100

    test_it init

---
+++ includes/tools/test_it.cpp
@@ -0,0 +1,11 @@
+#include "iterator.hpp"
+#include "lexicographical_compare.hpp"
+#include "pair.hpp"
+#include "type_traits.hpp"
+
+#include <iostream>
+
+int main (void)
+{
+	std::cout << "hola" << std::endl;
+}
